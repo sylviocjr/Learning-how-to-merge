dd01a38c163f58e87dbdd48acc371507dd9dad25
Sylvio Carneiro Junior <sylviocjr.dev@gmail.com>
2022-09-23 16:29:52 -0300

    Creation of this repository

---
+++ main.py
@@ -0,0 +1,14 @@
+'''
+File main.py.
+Objetivo: praticar uso das diversas branches usadas em ambiente
+colaborativo no git/github.
+
+Goal: practicing the use of different branches in a collaborative
+environment in git/github.
+
+Author: Sylvio Carneiro Júnior.
+Creation date: Sept. 29th, 2022.
+Email: sylviocjr.dev@gmail.com
+Github: https://github.com/sylviocjr
+LinkedIn: https://www.linkedin.com/in/sylvio-carneiro-junior/
+'''
